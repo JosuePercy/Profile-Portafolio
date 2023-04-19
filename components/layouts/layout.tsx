f5783cbe3aeb96c3a1443f4a0e3ca161968929ae
JosuePercy <josuehuallullo12@gmail.com>
2023-04-19 16:55:42 -0500

    feat: observer console display

--- components/layouts/layout.tsx
+++ components/layouts/layout.tsx
@@ -1,7 +1,13 @@
 
+import { useEffect } from "react"
 import { NavBar } from "../ui"
 
 export const Layout = () => {
+    useEffect(() => {
+        console.log("Width: " + window.innerWidth)
+        console.log("Height: " + window.innerHeight)
+    }, [])
+
     return (
         <>
             <NavBar />
