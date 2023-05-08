34578c51662f55d92a74e0c65f902e8aaa819456
JosuePercy <josuehuallullo12@gmail.com>
2023-05-08 18:52:00 -0500

    translate menu, shared

--- components/ui/button/buttomConfetti/ButtomConfetti.tsx
+++ components/ui/button/buttomConfetti/ButtomConfetti.tsx
@@ -30,7 +30,7 @@ export const ButtomConfetti = () => {
       spread: 360,
       origin: {
         x: 0.5,
-        // since they fall down, start a bit higher than randonshared
+        // since they fall down, start a bit higher than randonshared firts top
         y: 0.4
       }
     });
