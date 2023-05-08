3e3cab8195631b2ab0a946d4187199960e9149a3
JosuePercy <josuehuallullo12@gmail.com>
2023-05-08 18:51:32 -0500

    Shared update estructura

--- components/ui/button/buttomConfetti/ButtomConfetti.tsx
+++ components/ui/button/buttomConfetti/ButtomConfetti.tsx
@@ -30,7 +30,7 @@ export const ButtomConfetti = () => {
       spread: 360,
       origin: {
         x: 0.5,
-        // since they fall down, start a bit higher than randongg
+        // since they fall down, start a bit higher than randonshared
         y: 0.4
       }
     });
