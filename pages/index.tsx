d672ddeb837ce8d5b80d07d7f2ce1709321050ca
JosuePercy <josuehuallullo12@gmail.com>
2023-05-09 11:43:47 -0500

    Agregacion de tecgnolias

--- pages/index.tsx
+++ pages/index.tsx
@@ -100,7 +100,12 @@ const HomePage = () => {
               technologies={
                 [
                   'https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/html5/html5-plain.svg',
-                  'https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/react/react-original.svg'
+                  'https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/materialui/materialui-original.svg',
+                  'https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/nextjs/nextjs-original.svg',
+                  'https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/typescript/typescript-original.svg',
+                  'https://images.opencollective.com/nextui/93858ec/logo/256.png',
+                  'https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/css3/css3-plain.svg'
+
                 ]
               } />
 
