39cb8b76bae955585d8e6d053a6655cef1705498
JosuePercy <josuehuallullo12@gmail.com>
2023-05-12 11:33:18 -0500

    update

--- components/layouts/layout.tsx
+++ components/layouts/layout.tsx
@@ -5,12 +5,12 @@ type Props = {
     children: any
 }
 export const Layout = ({ children }: Props) => {
-    // useEffect(() => {
-    //     console.log("Width : movil: App" + window.innerWidth)
-    //     console.log("Height : movil App" + window.innerHeight)
-    //     alert("Width: " + window.innerWidth)
-    //     alert("Height: " + window.innerHeight)
-    // }, [])
+    useEffect(() => {
+        console.log("Width : movil: App" + window.innerWidth)
+        console.log("Height : movil App" + window.innerHeight)
+        alert("Width: " + window.innerWidth)
+        alert("Height: " + window.innerHeight)
+    }, [])
 
     return (
         <>
