35cbc825e5aebcbc0a6293e18721af8c95d9c8e5
JosuePercy <josuehuallullo12@gmail.com>
2023-08-31 11:18:40 -0500

    comment preload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Preloader />
+      {/* <Preloader /> */}
       <Component {...pageProps} />
     </>
   )
